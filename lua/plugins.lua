eb2659c3206792e8c480732ca1fd5b1f975d48f2
RaderH2O <raderh2ogt@gmail.com>
2024-10-02 22:10:43 +0330

    Added 2 colorschemes, everforest and gruvbox

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,21 @@
 local plugins = {
-	-- colorscheme
+	-- colorschemes
+	{
+		"neanias/everforest-nvim",
+		version = false,
+		lazy = false,
+		priority = 1000, -- make sure to load this before all the other start plugins
+		-- Optional; default configuration will be used if setup isn't called.
+		config = function()
+			require("everforest").setup({
+				-- Your config here
+			})
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000
+	},
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
